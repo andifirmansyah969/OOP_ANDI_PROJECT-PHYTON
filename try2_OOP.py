3a3ccab57d191c743bc99b71fd0ffd3b260b8ab4
andifirmansyah969 <andif9593@gmail.com>
2026-04-02 09:16:56 +0700

    Add files via upload

---
+++ try2_OOP.py
@@ -0,0 +1,2 @@
+print("satan");print("beelz");print("lucifer");print("noir")
+print("wrath");print("sloth");print("lust");print("greed")
---
+++ tugas1_OOP.py
@@ -0,0 +1,5 @@
+a="XXI"
+b=3
+c=18
+
+print("Hallo, Nama Saya Yolanda Dari Kelas",a,"rpl",b,"umur",c,"tahun")
