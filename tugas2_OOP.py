d86a8815a819d1281c49b9198d3350c55ff9ab54
andifirmansyah969 <andif9593@gmail.com>
2026-04-07 12:03:36 +0700

    Add files via upload

---
+++ tugas2_OOP.py
@@ -0,0 +1,11 @@
+judul="Judul Cerita:Toilet Angker Sekolah"
+tokoh="Pembuat Cerita Fiksi:Britayu"
+a="Andi"
+t="Sekolah"
+w="Pagi Hari"
+
+teks= "Cerita Horor"
+print(teks.center(35))
+print(judul)
+print(tokoh)
+print("pada",w,"josua akan pergi ke",t,",bersama teman temanya,ia bersiap²,untuk pergi ke",t,",dan sebelum temannya meninggalkannya. ia pun bergegas keluar dari pintu rumah setelah itu ternyata temannya sudah dari tadi menunggunya temannya ialah bernama",a,"dan iko.josua",a,"dan iko pun pergi ke",t,"naik sepedah masimg masing")
