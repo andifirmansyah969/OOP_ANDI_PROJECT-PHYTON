264b26a4b711f9700081cfdec7e6c6fbff58b3a3
andifirmansyah969 <andif9593@gmail.com>
2026-04-09 08:30:24 +0700

    Add files via upload

---
+++ try5_OOP.py
@@ -0,0 +1,19 @@
+a = 7
+
+a+=2
+print("a+=2=",a)
+print("\n")
+a-=6
+print("a-=6=",a)
+print("\n")
+a*=4
+print("a*=4=",a)
+print("\n")
+a/=2
+print("a/=2=",a)
+print("\n")
+a%=4
+print("a%=4=",a)
+print("\n")
+a**=2
+print("a**=4=",a)
