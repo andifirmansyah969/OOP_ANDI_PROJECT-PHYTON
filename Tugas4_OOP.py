68b97a0336a383759d47f70ec8bd360af8544f18
andifirmansyah969 <andif9593@gmail.com>
2026-04-23 09:06:18 +0700

    Add files via upload

---
+++ Tugas4_OOP.py
@@ -0,0 +1,14 @@
+nama=input("Masukan Nama Kamu:")
+print("halo",nama)
+
+umur=input("Masukan Umur:")
+print("Umur Saya",umur)
+
+no_hp=input("No hp saya:")
+print(no_hp)
+
+email=input("Email saya:")
+print(email)
+
+alamat=input("Alamat:")
+print(alamat)
